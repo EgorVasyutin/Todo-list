0509284eb66a827ac1ca406b2958190642665669
Egor <vaspovas@gmail.com>
2022-06-30 18:54:18 +0300

    сделал модальное окно

--- src/main.js
+++ src/main.js
@@ -1,2 +1,12 @@
-const div = document.getElementById("div");
-div.innerText = "Hello world";
+const modal = $.modal();
+
+const buttonYes = document.querySelector(".modal__button--yes");
+const buttonNo = document.querySelector(".modal__button--no");
+const buttonClose = document.querySelector(".modal__close");
+
+const closeModalWindow = () => {
+  modal.close();
+};
+buttonYes.onclick = closeModalWindow;
+buttonNo.onclick = closeModalWindow;
+buttonClose.onclick = closeModalWindow;
